9662439d8eab250313f208b95f382e5667963349
Vanush Grigoryan <grigvanush@gmail.com>
2020-07-27 18:25:07 +0200

    Initial Commit

---
+++ Hero/HeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroApp.swift
+//  Hero
+//
+//  Created by Vanush Grigoryan on 7/27/20.
+//
+
+import SwiftUI
+
+@main
+struct HeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
