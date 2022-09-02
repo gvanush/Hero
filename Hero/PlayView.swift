9a8690870e9cf96b2d2d7cf85891e0c0ae3f32b6
Vanush Grigoryan <grigvanush@gmail.com>
2022-09-02 21:31:50 +0200

    Added home swipe to dismiss play view

--- Hero/PlayView.swift
+++ Hero/PlayView.swift
@@ -23,11 +23,53 @@ class PlayViewModel: ObservableObject {
 struct PlayView: View {
     
     @ObservedObject var model: PlayViewModel
+    
     @Environment(\.presentationMode) private var presentationMode
+    @GestureState private var isDragging = false
+    @State private var dragValue: DragGesture.Value?
+    @State private var shouldCheckForExit = true
+    
     
     var body: some View {
-        SPTView(scene: model.scene, clearColor: UIColor.lightGray.mtlClearColor, viewCameraObject: model.viewCameraObject, lookCategories: LookCategories.userCreated.rawValue)
+        GeometryReader { geometry in
+            Group {
+                GeometryReader { sptViewGeometry in
+                    SPTView(scene: model.scene, clearColor: UIColor.lightGray.mtlClearColor, viewCameraObject: model.viewCameraObject, lookCategories: LookCategories.userCreated.rawValue)
+                        .gesture(dragGesture(geometry: sptViewGeometry, bottomSafeAreaInset: geometry.safeAreaInsets.bottom))
+                        .defersSystemGestures(on: .all)
+                }
+            }
             .ignoresSafeArea()
             .statusBarHidden()
+        }
+        .onChange(of: isDragging) { newValue in
+            if !newValue {
+                shouldCheckForExit = true
+                dragValue = nil
+            }
+        }
+    }
+    
+    func dragGesture(geometry: GeometryProxy, bottomSafeAreaInset: CGFloat) -> some Gesture {
+        DragGesture(minimumDistance: 0.0)
+            .updating($isDragging, body: { _, state, _ in
+                state = true
+            })
+            .onChanged({ newDragValue in
+                if let value = dragValue {
+                    let delta = CGSize(width: newDragValue.translation.width - value.translation.width, height: newDragValue.translation.height - value.translation.height)
+                    if delta.height > 0.0 || abs(delta.height) < abs(delta.width) {
+                        shouldCheckForExit = false
+                    }
+                    dragValue = newDragValue
+                }
+            })
+            .onEnded({ value in
+                let bounds = geometry.frame(in: .local)
+                let exitStartRect = bounds.inset(by: .init(top: bounds.height - bottomSafeAreaInset, left: 0.0, bottom: 0.0, right: 0.0))
+                if shouldCheckForExit && exitStartRect.contains(value.startLocation) {
+                    presentationMode.wrappedValue.dismiss()
+                }
+            })
     }
 }
