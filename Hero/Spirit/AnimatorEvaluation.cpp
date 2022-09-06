a5ca6949b597c5de6b37d0914f4fd9bf9b0a6669
Vanush Grigoryan <grigvanush@gmail.com>
2022-09-06 06:52:22 +0200

    Bug fixes and preparations

---
+++ Hero/Spirit/AnimatorEvaluation.cpp
@@ -0,0 +1,8 @@
+//
+//  AnimatorEvaluation.cpp
+//  Hero
+//
+//  Created by Vanush Grigoryan on 04.09.22.
+//
+
+#include "AnimatorEvaluation.hpp"
---
+++ Hero/Spirit/AnimatorEvaluation.h
@@ -0,0 +1,13 @@
+//
+//  AnimatorEvaluation.hpp
+//  Hero
+//
+//  Created by Vanush Grigoryan on 04.09.22.
+//
+
+#ifndef AnimatorEvaluation_hpp
+#define AnimatorEvaluation_hpp
+
+#include <stdio.h>
+
+#endif /* AnimatorEvaluation_hpp */
