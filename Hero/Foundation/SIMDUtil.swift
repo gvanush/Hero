acf8f87743b6c50d70e0ba3098f8041a377befa7
Vanush Grigoryan <grigvanush@gmail.com>
2022-12-23 22:02:26 +0100

    Cartesian Z animation bug fix

--- Hero/Foundation/SIMDUtil.swift
+++ Hero/Foundation/SIMDUtil.swift
@@ -12,7 +12,7 @@ extension SIMD3 where Scalar == Float {
     static let right = Self(1.0, 0.0, 0.0)
     static let down = Self(0.0, -1.0, 0.0)
     static let up = Self(0.0, 1.0, 0.0)
-    static let backward = Self(0.0, 0.0, -1.0)
-    static let forward = Self(0.0, 0.0, 1.0)
+    static let backward = Self(0.0, 0.0, 1.0)
+    static let forward = Self(0.0, 0.0, -1.0)
 }
 
