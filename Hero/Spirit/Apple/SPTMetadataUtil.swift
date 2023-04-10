ff4892056a04e2ebe0835ac458d45c6f979a9b6a
Vanush Grigoryan <grigvanush@gmail.com>
2023-04-10 10:45:07 +0200

    iOS SDK bump related adjustments

--- Hero/Spirit/Apple/SPTMetadataUtil.swift
+++ Hero/Spirit/Apple/SPTMetadataUtil.swift
@@ -28,7 +28,7 @@ extension SPTMetadata {
                 ptr.withMemoryRebound(to: CChar.self, capacity: Int(kSPTMetadataNameMaxLength) + 1) { charPtr in
                     newValue.utf8CString.withUnsafeBufferPointer { sourceCharPtr in
                         let length = min(sourceCharPtr.count, Int(kSPTMetadataNameMaxLength))
-                        charPtr.assign(from: sourceCharPtr.baseAddress!, count: length)
+                        charPtr.update(from: sourceCharPtr.baseAddress!, count: length)
                         charPtr[length] = 0
                     }
                 }
